60675644b7b71f071d47d15f3724ca1ef4143cb1
ImtiazEpu <imtiazepu@programmer.net>
2019-10-01 16:11:32 +0600

    metabox:meta value saves & get serialized way.

--- inc/MetaBox.php
+++ inc/MetaBox.php
@@ -4,6 +4,8 @@
 	 * Kick-in Class MetaBox
 	 */
 	class MetaBox {
+		
+		
 		/**
 		 * MetaBox constructor.
 		 */
@@ -12,6 +14,7 @@
 			add_action( 'save_post', array( $this, 'opd_save_metabox' ) );
 		}
 		//End method constructor
+		
 		
 		/**
 		 * Add metabox
@@ -26,6 +29,7 @@
 		}
 		//End method opd_add_metabox
 		
+		
 		/**
 		 * Save meta value
 		 *
@@ -34,32 +38,38 @@
 		 * @return mixed
 		 */
 		public function opd_save_metabox( $post_id ) {
-			
-			
-			$text_field  = isset( $_POST['opd_text'] ) ? $_POST['opd_text'] : '';
-			$email_field = isset( $_POST['opd_email'] ) ? $_POST['opd_email'] : '';
-			$is_favorite = isset( $_POST['opd_is_favorite'] ) ? $_POST['opd_is_favorite'] : 0;
-			$colors      = isset( $_POST['opd_color'] ) ? $_POST['opd_color'] : array();
-			$color       = isset( $_POST['opd_clr'] ) ? $_POST['opd_clr'] : '';
-			$fav_color   = isset( $_POST['opd_fav_color'] ) ? $_POST['opd_fav_color'] : '';
-			$multi_color = isset( $_POST['opd_multi_color'] ) ? $_POST['opd_multi_color'] : '';
+			$opdonmeta   = isset( $_POST['opdmeta'] ) ? $_POST['opdmeta'] : array();
+			$text_field  = isset( $opdonmeta['opd_text'] ) ? $opdonmeta['opd_text'] : '';
+			$email_field = isset( $opdonmeta['opd_email'] ) ? $opdonmeta['opd_email'] : '';
+			$is_favorite = isset( $opdonmeta['opd_is_favorite'] ) ? $opdonmeta['opd_is_favorite'] : 0;
+			$colors      = isset( $opdonmeta['opd_color'] ) ? $opdonmeta['opd_color'] : array();
+			$color       = isset( $opdonmeta['opd_clr'] ) ? $opdonmeta['opd_clr'] : '';
+			$fav_color   = isset( $opdonmeta['opd_fav_color'] ) ? $opdonmeta['opd_fav_color'] : '';
+			$multi_color = isset( $opdonmeta['opd_multi_color'] ) ? $opdonmeta['opd_multi_color'] : array();
 			
 			
 			$text_field  = sanitize_text_field( $text_field );
 			$email_field = sanitize_text_field( $email_field );
 			
+			$multi_select_checked = array();
+			$multi_select_checked['opd_is_favorite'] = $is_favorite;
+			$multi_select_checked['opd_color'] = $colors;
+			$multi_select_checked['opd_clr'] = $color;
+			$multi_select_checked['opd_fav_color'] = $fav_color;
+			$multi_select_checked['opd_multi_color'] = $multi_color;
 			
 			update_post_meta( $post_id, '_opd_text_field', $text_field );
 			update_post_meta( $post_id, '_opd_email_field', $email_field );
-			update_post_meta( $post_id, '_opd_is_favorite', $is_favorite );
-			update_post_meta( $post_id, '_opd_color', $colors );
-			update_post_meta( $post_id, '_opd_clr', $color );
-			update_post_meta( $post_id, '_opd_fav_color', $fav_color );
-			update_post_meta( $post_id, '_opd_multi_color', $multi_color );
+			//update_post_meta( $post_id, '_opd_is_favorite', $is_favorite );
+			//update_post_meta( $post_id, '_opd_color', $colors );
+			//update_post_meta( $post_id, '_opd_clr', $color );
+			//update_post_meta( $post_id, '_opd_fav_color', $fav_color );
+			//update_post_meta( $post_id, '_opd_multi_color', $multi_color );
 			
-			
+			update_post_meta( $post_id, '_opd_select_checked', $multi_select_checked );
 		}
 		//End method opd_save_metabox
+		
 		
 		/**
 		 * Meta Box callback function for rendering output
@@ -72,11 +82,11 @@
 			
 			$text_field  = get_post_meta( $post_id, '_opd_text_field', true );
 			$email_field = get_post_meta( $post_id, '_opd_email_field', true );
-			$is_favorite = get_post_meta( $post_id, '_opd_is_favorite', true );
-			$save_colors = get_post_meta( $post_id, '_opd_color', true );
-			$save_color  = get_post_meta( $post_id, '_opd_clr', true );
-			$fav_color   = get_post_meta( $post_id, '_opd_fav_color', true );
-			$multi_color = get_post_meta( $post_id, '_opd_multi_color', true );
+			$is_favorite = get_post_meta( $post_id, '_opd_select_checked', true );
+			$save_colors = get_post_meta( $post_id, '_opd_select_checked', true );
+			$save_color  = get_post_meta( $post_id, '_opd_select_checked', true );
+			$fav_color   = get_post_meta( $post_id, '_opd_select_checked', true );
+			$multi_color = get_post_meta( $post_id, '_opd_select_checked', true );
 			
 			$text_label     = __( 'Text field', 'optionsdemo' );
 			$email_label    = __( 'Email field', 'optionsdemo' );
@@ -94,26 +104,26 @@
 				'pink',
 				'black'
 			];
-			$checked = $is_favorite == 1 ? 'checked' : '';
+			$checked = $is_favorite['opd_is_favorite'] == 1 ? 'checked' : '';
 			
 			$metabox_html = <<<EOD
 			<!--Text field-->
             <p>
                 <label for="opd_text">{$text_label}:</label>
-                <input type="text" name="opd_text" id="opd_text" value="{$text_field}">
+                <input type="text" name="opdmeta[opd_text]" id="opd_text" value="{$text_field}">
             </p>
             <br/>
             <!--email field-->
             <p>
                 <label for="opd_email">{$email_label}:</label>
-                <input type="email" name="opd_email" id="opd_email" value="{$email_field}">
+                <input type="email" name="opdmeta[opd_email]" id="opd_email" value="{$email_field}">
             </p>
             <br/>
             <!--checkbox field-->
             <p>
                 <label for="opd_is_favorite">{$checkbox_label}:</label>
                 
-                <input type="checkbox" name="opd_is_favorite" id="opd_is_favorite" value="1" {$checked}>
+                <input type="checkbox" name="opdmeta[opd_is_favorite]" id="opd_is_favorite" value="1" {$checked}>
             </p>
 <br/>
 
@@ -127,12 +137,12 @@ EOD;
 			/*if ( ! is_array( $save_colors ) ) {
 				$save_colors = array();
 			}*/
-			foreach ( $colors as $color ) {
+			foreach ( $colors as $key => $color ) {
 				$color        = ucwords( $color );
-				$checked      = is_array( $save_colors ) && in_array( $color, $save_colors ) ? 'checked' : '';
+				$checked      = is_array( $save_colors['opd_color'] ) && in_array( $key, $save_colors['opd_color'] ) ? 'checked' : '';
 				$metabox_html .= <<<EOD
          
-                <input type="checkbox" name="opd_color[]" id="opd_color_{$color}" value="{$color}" {$checked}>
+                <input type="checkbox" name="opdmeta[opd_color][]" id="opd_color_{$color}" value="{$key}" {$checked}>
                 <label for="opd_color_{$color}">{$color}</label>
 EOD;
 			}
@@ -150,11 +160,11 @@ EOD;
 			
 			foreach ( $colors as $color ) {
 				$color        = ucwords( $color );
-				$checked      = ( $color == $save_color ) ? "checked='checked'" : '';
+				$checked      = ( $color == $save_color['opd_clr'] ) ? "checked='checked'" : '';
 				$metabox_html .= <<<EOD
          
                 
-                <input type="radio" name="opd_clr" id="opd_clr_{$color}" value="{$color}" {$checked}>
+                <input type="radio" name="opdmeta[opd_clr]" id="opd_clr_{$color}" value="{$color}" {$checked}>
                 <label for="opd_clr_{$color}">{$color} </label>
 EOD;
 			}
@@ -166,7 +176,7 @@ EOD;
 			$dropdown_html = '<option value="0">' . __( "Select a color", "optionsdemo" ) . '</option>';
 			foreach ( $colors as $color ) {
 				$selected = '';
-				if ( $color == $fav_color ) {
+				if ( $color == $fav_color['opd_fav_color'] ) {
 					$selected = 'selected';
 				}
 				$dropdown_html .= sprintf( '<option value="%s" %s>%s</option>', $color, $selected, $color );
@@ -176,8 +186,8 @@ EOD;
 			<br/>
 			<p>
 				<label for="opd_fav_color">{$select_color}</label>
-				<select name="opd_fav_color" id="opd_fav_color">
-					{$dropdown_html}
+				<select name="opdmeta[opd_fav_color]" id="opd_fav_color">
+				{$dropdown_html}
 				</select>
 			</p>
 EOD;
@@ -186,20 +196,20 @@ EOD;
 			//Multiple Select Fields
 			
 			$dropdown_html = '<option value="0">' . __( "Select a colors", "optionsdemo" ) . '</option>';
-			foreach ( $colors as $color ) {
+			foreach ( $colors as $key => $color ) {
 				$selected = '';
-				if ( is_array( $multi_color ) && in_array( $color, $multi_color ) ) {
+				if ( is_array( $multi_color['opd_multi_color'] ) && in_array( $key, $multi_color['opd_multi_color'] ) ) {
 					$selected = 'selected';
 				}
-				$dropdown_html .= sprintf( '<option value="%s" %s>%s</option>', $color, $selected, $color );
+				$dropdown_html .= sprintf( '<option value="%s" %s>%s</option>', $key, $selected, $color );
 			}
 			
 			$metabox_html .= <<<EOD
 			<br/>
 			<p>
 				<label for="opd_multi_color">{$select_colors}</label>
-				<select name="opd_multi_color[]" id="opd_multi_color" multiple>
-					{$dropdown_html}
+				<select name="opdmeta[opd_multi_color][]" id="opd_multi_color" multiple>
+				{$dropdown_html}
 				</select>
 			</p>
 EOD;
